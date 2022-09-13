39960d9a12147a7d1426e0548ede975ba2914791
luiskaiser <luiskaiser@t-online.de>
2022-09-13 17:28:54 -0500

    compare unet and tiramisu on tacc

--- bash_scripts/data_generation.sh
+++ bash_scripts/data_generation.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=data_generation_1
 #SBATCH --output=data_generation_1.out
 #SBATCH --nodes=1
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=500M
--- bash_scripts/train_tiramisu.sh
+++ bash_scripts/train_tiramisu.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=train_tiramisu_1
 #SBATCH --output=train_tiramisu_1.out
 #SBATCH --nodes=1
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
