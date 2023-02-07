bf76844cb1cde9243ee78e17070de09b038830b6
luiskaiser <luiskaiser@t-online.de>
2023-02-07 17:54:30 -0600

    start training parareal

--- models/optimization_models/optimize_solution.py
+++ models/optimization_models/optimize_solution.py
@@ -1,31 +1,43 @@
+from analysis.utils_analysis import get_solver_solution, one_iteration_pseudo_spectral
 from analysis.visualize_results.visualize_training import visualize_wavefield
 import random
 import torch
-from generate_data.initial_conditions import get_velocities, initial_condition_gaussian
+from generate_data.initial_conditions import get_velocities, initial_condition_gaussian, get_velocity_crop
 from generate_data.utils_wave import WaveEnergyComponentField_end_to_end, WaveSol_from_EnergyComponent_tensor, \
     WaveEnergyComponentField_tensor, WaveEnergyField_tensor
 from generate_data.wave_propagation import pseudo_spectral_tensor
 from models.model_utils import sample_label_normal_dist, get_wavefield, round_loss, compute_loss, compute_loss2
 from generate_data.utils_wave import WaveSol_from_EnergyComponent
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-from models.optimization_models.parallel_scheme import parareal_scheme, one_iteration_pseudo_spectral_tensor
+from models.optimization_models.parallel_scheme import parareal_scheme, one_iteration_pseudo_spectral_tensor, \
+    parareal_scheme2
 import matplotlib.pyplot as plt
 import numpy as np
+
+
 
 def model_optimization_solution(data, model, loss_f, label_distr_shift, mode, i, epoch,
                                 vis_path, vis_save, optimization_type, multi_step, res):
 
     batch_size, n_snaps, c, w, h = data.shape
     loss_list = []
-
     if mode == "train":
         # random.choices(range(n_snaps - 2), k=n_snaps)
+        vel = torch.from_numpy(get_velocity_crop(256, 1, "diagonal", "periodic", "parareal", "../")).squeeze()
+        u_0 = torch.concat([initial_condition_gaussian(vel,128,"absorbing", 256,"parareal","parareal"), vel.unsqueeze(dim=0).unsqueeze(dim=0)], dim=1).squeeze()  # c x w x h
+        u_n_k = u_0[:3].clone()
+        fine_sol = torch.zeros([1, 8, 3, 256, 256])
+
+        for s in range(8):
+            fine_sol[0, s] = u_n_k
+            u_n_k = torch.concat([u_n_k, vel.unsqueeze(dim=0)], dim=0).unsqueeze(dim=0)
+            u_n_k = one_iteration_pseudo_spectral(u_n_k).squeeze()
 
         for input_idx in [0,0]:
             label_range = 5  # sample_label_normal_dist(input_idx, n_snaps, label_distr_shift, multi_step)
-
             if optimization_type == "parareal":
-                loss_list += parareal_scheme(model, input_idx, 4, label_range, loss_f, data)
+                loss_list += parareal_scheme2(model, input_idx, 2, label_range, loss_f, fine_sol, vel)
+                # loss_list += parareal_scheme(model, u_0.unsqueeze(dim=0), fine_sol)
 
             else:  # optimization_type == "procrustes"
                 pass
--- models/optimization_models/parallel_scheme.py
+++ models/optimization_models/parallel_scheme.py
@@ -1,32 +1,89 @@
 import torch
 from matplotlib import pyplot as plt
+from analysis.utils_analysis import one_iteration_pseudo_spectral
 from analysis.visualize_results.plot_training_optimization import plot_big_tensor
 from generate_data.utils_wave import WaveSol_from_EnergyComponent_tensor, WaveEnergyComponentField_tensor
 from generate_data.wave_propagation import pseudo_spectral_tensor
-from models.model_utils import smaller_crop, get_wavefield
-
+from models.model_utils import smaller_crop, get_wavefield, compute_loss
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def parareal_scheme(model, input_idx, n_parareal, label_range, loss_f, fine_tensor):
+
+def parareal_scheme(model, u_0, fine_sol, n_parareal = 2, n_snapshots = 7):
+
+    # u_n -> b x c x w x h
+        u_n = u_0.clone()
+        vel = u_n[:,3].clone().unsqueeze(dim=1)  # 1 x 1 x 500 x 500
+        batch_size, channel, width, height = u_n.shape  # 1 x 4 x 500 x 500
+        parareal_tensor = torch.zeros([n_parareal+1, n_snapshots, batch_size, channel-1, 128, 128])
+        big_tensor = torch.zeros([n_snapshots, batch_size, channel - 1, width, height])
+
+        # initial guess, first iteration without parareal
+        parareal_tensor[0, 0] = smaller_crop(u_n[:, :3].clone())
+        for n in range(n_snapshots-1):
+            u_n1 = model(u_n)  # 1 x 3 x 512 x 512
+            parareal_tensor[0,n+1] = smaller_crop(u_n1)
+            big_tensor[n+1] = u_n1
+            u_n = torch.cat((u_n1, vel), dim=1)
+
+        # parareal iterations: k = 1, 2, 3, 4
+        for k in range(1,n_parareal+1):
+            print(k)
+
+            big_tensor[0] = u_0[:, :3].clone()
+            parareal_tensor[k, 0] = smaller_crop(u_0[:, :3].clone())
+            parareal_terms = get_parareal_terms(model, big_tensor, n_snapshots, vel) # n_snapshots x b x c x w x h
+            new_big_tensor = torch.zeros([n_snapshots, batch_size, channel - 1, width, height])
+            new_big_tensor[0] = u_0[:, :3].clone()
+
+            for n in range(n_snapshots-1):
+                u_n_k1 = torch.cat((new_big_tensor[n], vel), dim=1)
+                u_n1_k1 = model(u_n_k1) + parareal_terms[n]
+                parareal_tensor[k, n+1] = smaller_crop(u_n1_k1)
+                new_big_tensor[n+1] = u_n1_k1
+
+            big_tensor = new_big_tensor.clone()
+        a(parareal_tensor, vel, fine_sol)
+        return parareal_tensor  # k x s x b x c x w x h
+
+
+def get_parareal_terms(model, big_pseudo_tensor, n_snapshots, vel):
+    # this can be done later computed in parallel
+
+    parareal_terms = torch.zeros(big_pseudo_tensor.shape) # n_snapshots x b x c x w x h
+
+    for s in range(n_snapshots):
+        parareal_terms[s] = compute_parareal_term(model, torch.cat([big_pseudo_tensor[s], vel], dim=1))
+
+    return parareal_terms
+
+def compute_parareal_term(model, u_n_k):
+
+    # u_n_k -> b x c x w x h
+
+    res_fine_solver = one_iteration_pseudo_spectral(u_n_k) #one_iteration_velocity_verlet(u_n_k)
+    res_model = model(u_n_k)  # procrustes_optimization(model(u_n_k), res_fine_solver)
+
+    return res_fine_solver - res_model
+
+
+def parareal_scheme2(model, input_idx, n_parareal, label_range, loss_f, fine_tensor, vel):
     # data -> b x n_snaps x 3 x w x h
     n_parareal = 2
-    f_delta_x = 2./128.
     # data setup
     u_0 = fine_tensor[:, input_idx]  # b x 3 x w x h
-    vel = fine_tensor[:, 0, 2].unsqueeze(dim=0)
-    u_n = u_0.clone()  # b x 3 x w x h
+    vel = vel.unsqueeze(dim=0).unsqueeze(dim=0)
+    u_n = torch.cat((u_0.clone(), vel), dim=1)  # b x 4 x w x h
     batch_size, channel, width, height = u_0.shape  # b x 4 x 256 x 256
-    big_tensor = torch.zeros([n_parareal+1, batch_size, label_range - input_idx + 1, 2, width, height]).to(device)
+    big_tensor = torch.zeros([n_parareal+1, batch_size, label_range - input_idx + 1, channel, width, height]).to(device)
     loss_list = []
 
     # initial guess, first iteration without parareal
-    big_tensor[0, :, 0] = u_0[:,:2].clone()
+    big_tensor[0, :, 0] = u_0[:,:3].clone()
 
     for n in range(label_range - input_idx):
-        u_n = up3(u_n, vel)
         u_n1 = model(u_n)  # b x c x w x h
         loss_list.append(loss_f(smaller_crop(u_n1),
-                                smaller_crop(up3(fine_tensor[:, n + 1,:2], vel))))
+                                smaller_crop(fine_tensor[:, n + 1,:3])))
 
         big_tensor[0, :, n+1] = u_n1.clone().detach()
         u_n = torch.cat((u_n1, vel), dim=1)
@@ -35,21 +92,12 @@ def parareal_scheme(model, input_idx, n_parareal, label_range, loss_f, fine_tens
     for k in range(1,n_parareal+1):
         print("--- parareal it",k)
         res_fine, res_model = get_optimizing_terms(model, big_tensor[k-1].to(device), vel.to(device), input_idx, label_range)  # bs x n_snaps x c x w x h
-        new_big_tensor = torch.zeros([batch_size, label_range - input_idx + 1, 2, width, height]).to(device)
-        new_big_tensor[:, 0] = u_0[:, :2].clone()
+        new_big_tensor = torch.zeros([batch_size, label_range - input_idx + 1, channel, width, height]).to(device)
+        new_big_tensor[:, 0] = u_0[:, :3].clone()
 
         for n in range(label_range - input_idx):
             u_n_k1 = torch.cat((new_big_tensor[:, n], vel), dim=1)
             u_n1_k1 = model(u_n_k1) + res_fine[:,n] - res_model[:,n]
-
-            fig = plt.figure(figsize=(30, 15))
-            ax = fig.add_subplot(1,3,1)
-            ax.imshow(get_wavefield(model(u_n_k1).clone().detach(), vel.squeeze()))
-            ax1 = fig.add_subplot(1, 3, 2)
-            ax1.imshow(get_wavefield(res_fine[0,n].unsqueeze(dim=0).clone().detach(), vel.squeeze()))
-            ax2 = fig.add_subplot(1, 3, 3)
-            ax2.imshow(get_wavefield(res_model[0,n].unsqueeze(dim=0).clone().detach(), vel.squeeze()))
-            plt.show()
 
             # we train only when parareal it <= curr snapshot; since for all other cases, pseudo-spectral is applied n times, so close perfect result already
             # if k <= n: loss_list.append(loss_f(smaller_crop(u_n1_k1),
@@ -75,13 +123,13 @@ def get_optimizing_terms(model, big_pseudo_tensor, vel, input_idx, label_range):
     #model.eval()
     #with torch.no_grad():
     for s in range(label_range - input_idx):
-        res_fine[:,s], res_model[:,s] = compute_parareal_term(model, torch.cat([big_pseudo_tensor[:,s], vel], dim=1))
+        res_fine[:,s], res_model[:,s] = compute_parareal_term2(model, torch.cat([big_pseudo_tensor[:,s], vel], dim=1))
 
     #model.train()
     return res_fine, res_model
 
 
-def compute_parareal_term(model, u_n_k):
+def compute_parareal_term2(model, u_n_k):
     # u_n_k -> b x c x w x h
 
     res_model = model(u_n_k)  # b x 3 x w x h
@@ -90,7 +138,7 @@ def compute_parareal_term(model, u_n_k):
     return res_fine_solver, res_model
 
 
-def one_iteration_pseudo_spectral_tensor(u_n_k, f_delta_x = 2./128., f_delta_t = (2./128.) / 50., delta_t_star = .06):
+def one_iteration_pseudo_spectral_tensor(u_n_k, f_delta_x = 2./128., f_delta_t = (2./128.) / 20., delta_t_star = .06):
     # u_n_k -> b x c x w x h
     u, u_t = WaveSol_from_EnergyComponent_tensor(u_n_k[:, 0],
                                                  u_n_k[:, 1],
@@ -106,21 +154,18 @@ def one_iteration_pseudo_spectral_tensor(u_n_k, f_delta_x = 2./128., f_delta_t =
     return torch.stack([u_x, u_y, u_t_c], dim=1)
 
 
-def one_iteration_pseudo_spectral_tensor_ut(u_n_k, f_delta_x = 2./128., f_delta_t = (2./128.) / 50., delta_t_star = .06):
-    # u_n_k -> b x c x w x h
 
-    u_prop, u_t_prop = pseudo_spectral_tensor(u_n_k[:,0], u_n_k[:,1], u_n_k[:, 2], f_delta_x, f_delta_t, delta_t_star)
-    u_x, u_y, u_t_c = WaveEnergyComponentField_tensor(u_prop,
-                                                      u_t_prop,
-                                                      u_n_k[:, 2], f_delta_x)
-    return torch.stack([u_x, u_y, u_t_c], dim=1)
+def a(parareal_tensor, vel, fine_sol):
 
+    fig = plt.figure(figsize=(35, 15))
 
-def up3(u_n, vel, f_delta_x = 2./128.):
-    u, ut = u_n[:,0], u_n[:,1]
-    a,b,c = WaveEnergyComponentField_tensor(u, ut, vel, f_delta_x)
-    return torch.stack([a,b,c], dim=1)
+    for k in range(parareal_tensor.shape[0]):
+        for s in range(parareal_tensor.shape[1]):
+            wave_field = get_wavefield(parareal_tensor[k,s], smaller_crop(vel))
+            ax = fig.add_subplot(3, 11, 11 * k + s + 1)
+            pos = ax.imshow(wave_field)
+            if s != 0:
+                ax.set_title(compute_loss(parareal_tensor[k,s], smaller_crop(fine_sol[0,s].unsqueeze(dim=0)), smaller_crop(vel)))
+            plt.axis('off')
 
-
-
-
+    plt.show()
